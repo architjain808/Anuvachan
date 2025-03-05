209a4cfcb87681aa317d7a3d06d207aa9baed752
architjain808 <architjain808@gmail.com>
2025-03-05 10:20:08 +0530

    [fix] for deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-import { AppRoutingModule } from 'src/app/app-routing.module';
-describe('AppComponent', () => {
+import { AppRoutingModule } from "src/app/app-routing.module";
+describe("AppComponent", () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [AppRoutingModule],
@@ -10,24 +10,24 @@ describe('AppComponent', () => {
     })
   );
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'translation-tool'`, () => {
+  it(`should have as title 'Anuvachan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('translation-tool');
+    expect(app.title).toEqual("Anuvachan");
   });
 
-  xit('should render initial title', () => {
+  xit("should render initial title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'translation-tool app is running!'
+    expect(compiled.querySelector(".content span")?.textContent).toContain(
+      "Anuvachan app is running!"
     );
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TranslationTableComponent } from './Components/translation-table/translation-table.component';
-import { TableModule } from 'primeng/table';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CheckboxModule } from 'primeng/checkbox';
-import { ToastModule } from 'primeng/toast';
-import { RippleModule } from 'primeng/ripple';
-import { MessageService } from 'primeng/api';
-import { FileUploadModule } from 'primeng/fileupload';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { RetryInterceptor } from './services/retry-interceptor';
-import { APP_BASE_HREF } from '@angular/common';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TranslationTableComponent } from "./Components/translation-table/translation-table.component";
+import { TableModule } from "primeng/table";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CheckboxModule } from "primeng/checkbox";
+import { ToastModule } from "primeng/toast";
+import { RippleModule } from "primeng/ripple";
+import { MessageService } from "primeng/api";
+import { FileUploadModule } from "primeng/fileupload";
+import { ProgressBarModule } from "primeng/progressbar";
+import { RadioButtonModule } from "primeng/radiobutton";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { RetryInterceptor } from "./services/retry-interceptor";
+import { APP_BASE_HREF } from "@angular/common";
 @NgModule({
   declarations: [AppComponent, TranslationTableComponent],
   imports: [
@@ -48,7 +48,7 @@ import { APP_BASE_HREF } from '@angular/common';
       useClass: RetryInterceptor,
       multi: true,
     },
-    { provide: APP_BASE_HREF, useValue: `/translation-tool/` },
+    { provide: APP_BASE_HREF, useValue: `/Anuvachan/` },
   ],
   bootstrap: [AppComponent],
 })
